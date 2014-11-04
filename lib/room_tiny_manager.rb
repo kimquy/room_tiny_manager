53c5c1733579ee3dba5f9312b331102e75e7ea66
Long Nguyen <longn@vinagility.vn>
2014-11-04 18:43:51 +0700

    Update the solution

--- lib/room_tiny_manager.rb
+++ lib/room_tiny_manager.rb
@@ -14,25 +14,51 @@ module RoomTinyManager
     ## FIXME: I added my access_token
     ## Can be more general, that some one use the gem can set their access token
     ROOMS_END_POINT = "https://api.roomorama.com/v1.0/rooms.json"
+    HOST_ROOM_END_POINT = "https://api.roomorama.com/v1.0/host/rooms.json"
     ACCESS_TOKEN = "0WGRIQTVZyf9a6kudI8zmq5QAmMCGzRlhD1jq97T54"
 
-    def initialize
+    def initialize(properties_input)
+      @properties = parse_input(properties_input)
     end
 
-    def self.get_properties
-      raw_result = RestClient.get(ROOMS_END_POINT, {access_token: ACCESS_TOKEN})
-      JSON.parse(raw_result)
+    def get_properties
+      @properties
     end
 
-    def self.room_by_city(city=nil)
-      raise "Missing argument: city" if city.nil?
-      raw_result = RestClient.get(ROOMS_END_POINT, { params: {access_token: ACCESS_TOKEN, destination: city } })
-      JSON.parse(raw_result)
-    end
-
+    # Post new room to the following end_point "https://api.roomorama.com/v1.0/host/rooms.json"
     def post_a_room
-      
+      raw_result = RestClient.get(HOST_ROOM_END_POINT, { params: @properties.first.merge(access_token: ACCESS_TOKEN) })
+      JSON.parse(raw_result)
     end
+
+    # def self.room_by_city(city=nil)
+    #   raise "Missing argument: city" if city.nil?
+    #   raw_result = RestClient.get(ROOMS_END_POINT, { params: {access_token: ACCESS_TOKEN, destination: city } })
+    #   JSON.parse(raw_result)
+    # end
+
+    private
+      def parse_input(properties)
+        properties = JSON.parse(properties)["properties"]
+
+        rooms = []
+        properties.each do |property|
+          map_p = {
+            address: property["property_address"],
+            apartment_number: property["apartment_no"],
+            postal_code: property["zip_code"],
+            city: property["town"],
+            country: property["country"],
+            description: property["property_description"],
+            number_of_bedrooms: property["bedrooms_no"],
+            max_guests: property["max_people"],
+            internal_id: property["source_id"]
+          }
+          rooms << map_p
+        end
+        
+        rooms
+      end
 
   end
 end
--- spec/room_tiny_manager/fetcher_spec.rb
+++ spec/room_tiny_manager/fetcher_spec.rb
@@ -2,31 +2,47 @@ require 'spec_helper'
 
 module RoomTinyManager
   describe Fetcher do
+
+    let(:sigle_input) { "{\"properties\":[{\"property_address\":\"72 Central Park W\",\"apartment_no\":\"7\",\"zip_code\":\"8002\",\"town\":\"Zurich\",\"country\":\"Swiss\",\"property_description\":\"fake description of the property\",\"bedrooms_no\":\"1\",\"max_people\":\"2\",\"source_id\":\"68123\"}]}" }
+
+    let(:multiple_inputs) {
+      "{\"properties\":[{\"property_address\":\"72 Central Park W\",\"apartment_no\":\"7\",\"zip_code\":\"8002\",\"town\":\"Zurich\",\"country\":\"Swiss\",\"property_description\":\"fake description of the property\",\"bedrooms_no\":\"1\",\"max_people\":\"2\",\"source_id\":\"68123\"},
+      {\"property_address\":\"72 Central Park W\",\"apartment_no\":\"4\",\"zip_code\":\"8002\",\"town\":\"Saigon\",\"country\":\"Vietnam\",\"property_description\":\"fake description of the property\",\"bedrooms_no\":\"1\",\"max_people\":\"2\",\"source_id\":\"12345\"}
+      ]}"
+    }
+
     describe "#get_properties" do
 
-      let(:rooms) { RoomTinyManager::Fetcher.get_properties["result"] }
+      let(:fetcher1) { RoomTinyManager::Fetcher.new(sigle_input) }
+      let(:fetcher2) { RoomTinyManager::Fetcher.new(multiple_inputs) }
+
+      it "returns 1 property" do 
+        expect(fetcher1.get_properties.count).to eq(1)
+      end
 
       it "returns list of properties" do 
-        expect(rooms.length).to be > 0
+        expect(fetcher2.get_properties.count).to be > 1
       end
+
+
     end
 
-    describe "#room_by_city" do
-      let(:room) { RoomTinyManager::Fetcher.room_by_city("singapore")["result"].first } 
+    # describe "#room_by_city" do
+    #   let(:room) { RoomTinyManager::Fetcher.room_by_city("singapore")["result"].first } 
 
-      it "room must be in Singapore" do
-        expect(room["city"]).to eq("Singapore")
-      end 
+    #   it "room must be in Singapore" do
+    #     expect(room["city"]).to eq("Singapore")
+    #   end 
 
-      it "room must not from New York" do
-        expect(room["city"]).not_to eq("New York")
-      end
-    end
+    #   it "room must not from New York" do
+    #     expect(room["city"]).not_to eq("New York")
+    #   end
+    # end
 
 
-    describe "#post_a_room" do
-      pending 'Fill some test here'
-    end
+    # describe "#post_a_room" do
+    #   pending 'Fill some test here'
+    # end
 
   end
 end
